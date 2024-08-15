ba6e6e789e44f30116ab61e3b92e3600f9acd777
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-15 07:12:53 +0200

    Conditional Display of Difficulties

--- src/components/DisplayDifficulty/DisplayDifficulty.jsx
+++ src/components/DisplayDifficulty/DisplayDifficulty.jsx
@@ -3,7 +3,10 @@ import styleModule from './style.module.css';
 function DisplayDifficulty(props) {
     return (
         <div className={styleModule.container}>
-            Difficulty Set to : {props.difficulty}
+            {
+                props.difficulty ? 
+                `Difficulty Set to : ${props.difficulty}` : "No Difficulty Set"
+            }
         </div>
     )
 }
